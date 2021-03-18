496e3d083bf698b4bcf9f5ad606e1cb0f2517b37
Edissonal <edissonalonso@gmail.com>
2021-03-18 08:59:30 -0500

    paises app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    SharedModule,
+    PaisModule,
+    AppRoutingModule,
+    HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
